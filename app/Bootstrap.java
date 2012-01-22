04572641338441adece96afd3fd9a54fe18033ed
Richard Warburton <richard.warburton@gmail.com>
2012-01-22 21:53:49 +0000

    tidy

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -14,10 +14,10 @@ public class Bootstrap extends Job {
 			Fixtures.deleteDatabase();
 			Fixtures.loadModels("initial-data.yml");
 
-			User user = User.connect("bob@gmail.com", "secret");
-			for (int i = 0; i < 20; i++) {
-				new Post(user, "title"+i, "content"+i,"tease"+i).save();
-			}
+//			User user = User.connect("bob@gmail.com", "secret");
+//			for (int i = 0; i < 20; i++) {
+//				new Post(user, "title"+i, "content"+i,"tease"+i).save();
+//			}
 		}
 	}
 
